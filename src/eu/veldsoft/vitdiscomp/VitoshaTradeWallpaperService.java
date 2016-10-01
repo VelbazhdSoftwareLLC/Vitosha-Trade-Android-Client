7d8b46994d5293074c3043bde8bffe4ceafd0532
Todor Balabanov <todor.balabanov@gmail.com>
2016-10-01 12:01:33 +0300

    Wallpaper service was added.

---
+++ src/eu/veldsoft/vitdiscomp/VitoshaTradeWallpaperService.java
@@ -0,0 +1,14 @@
+package eu.veldsoft.vitdiscomp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.service.wallpaper.WallpaperService;
+
+public class VitoshaTradeWallpaperService extends WallpaperService {
+
+	@Override
+	public Engine onCreateEngine() {
+		return null;
+	}
+}
