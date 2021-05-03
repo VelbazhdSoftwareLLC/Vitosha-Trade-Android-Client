1515993e39c5510ee573008f5130b920d1b18e57
Todor Balabanov <todor.balabanov@gmail.com>
2021-05-03 19:03:47 +0300

    Project reorganization was done.

---
+++ Command-Line-Interface/src/main/java/eu/veldsoft/vitosha/trade/ConsolePredictor.java
@@ -0,0 +1,12 @@
+package eu.veldsoft.vitosha.trade;
+
+//import eu.veldsoft.vitosha.trade.dummy.InputData;
+
+/**
+ * Single entry point class for command line application interface.
+ */
+public class ConsolePredictor {
+    public static void main(String[] args){
+        System.out.println( "Hello ..." );
+    }
+}
